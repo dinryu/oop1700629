921a9b4e87250732dce5dec16274cd831aa18c59
유주열 <dinryu@naver.com>
2024-07-25 10:30:30 +0900

    Delete src directory

--- src/com/hanbit/opp/service/GradeService.java
+++
@@ -1,9 +0,0 @@
-package com.hanbit.opp.service;
-
-import com.hanbit.oop.domain.GradeBean;
-
-public interface GradeService {
-	public int calcTotal(GradeBean g);
-	public int calcAvg(int total);
-	public String getGrade(int avg);
-}
